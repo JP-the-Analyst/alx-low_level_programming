89afc4907f155e9ddb93ec0b79e80fdc99e45b2c
Chikwe Johnpaul <josemariajchikwe@gmail.com>
2023-07-17 03:29:20 -0700

    0x00 Hello World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - Main entry point
+* return - Ends program after success
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
