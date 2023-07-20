4ba4533a12290e3a6bbc2f48b8d7478fe6ecf9b0
Chikwe Johnpaul <josemariajchikwe@gmail.com>
2023-07-19 22:02:18 -0700

    0x02. C

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - Return absolute value
+ * @n: number
+ * Return: absolute number
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return ((-1 * n));
+	}
+}
